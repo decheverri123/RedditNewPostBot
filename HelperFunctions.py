689626161650670c905a43e9f1abd69c9c629dca
Daniel Betancur-Echeverri <danny@daniels-mbp.lan>
2020-04-03 11:01:03 -0700

    refactor to pep8 compliance

--- HelperFunctions.py
+++ HelperFunctions.py
@@ -6,27 +6,8 @@ import smtplib
 import ssl
 import local_settings
 
-def getNewPosts(subreddit):
-
-    reddit = praw.Reddit(client_id=local_settings.client_id,
-                     client_secret=local_settings.client_secret,
-                     password=local_settings.password,
-                     username="decheverri123",
-                     user_agent="test script")
-
-    currPost = []
-
-    for submission in reddit.subreddit(subreddit).new(limit=1):
-        link = "https://reddit.com{0}".format(submission.permalink)
-        if not currPost or currPost.pop()[0] != submission.id:
-            currPost.pop() if currPost else None
-            currPost.append([submission.id, submission.title, link])
-        else:
-            return None
-    
-    return currPost
         
-def sendEmail(id, title, link):
+def send_email(id, title, link):
 
     port = 465 
     context = ssl.create_default_context()
@@ -38,8 +19,8 @@ def sendEmail(id, title, link):
     message["Subject"] = title 
     message["From"] = sender_email
     message["To"] = receiver_email 
+    
     part1 = MIMEText(link, "plain")
-
     message.attach(part1)
 
     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
